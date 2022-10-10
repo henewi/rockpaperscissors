d437e40e61017b06fdb44bd2ad5eabe2cab90e5e
henewi <hpdunn2002@gmail.com>
2022-10-10 16:05:17 +0100

    random number gen

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-// Define the function 
+// Define the function for AI gen number
 // function to choose number between 1 and 3 
 // If number is 1 rock 2 paper 3 scissors
 // Ask user for input rock paper or scissors
@@ -13,3 +13,9 @@
 // if computer 3 and user 1, computer wins
 // if computer 3 and user 2, user wins
 // if computer 3 and user 3, draw
+
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+const computer = randomIntFromInterval(1,3)
