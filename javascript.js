2260caeb68e19df1e654c7a86c72b5a8e54a91ee
henewi <hpdunn2002@gmail.com>
2022-10-10 17:09:10 +0100

    first 2 if

--- javascript.js
+++ javascript.js
@@ -4,15 +4,15 @@
 // Ask user for input rock paper or scissors
 // If user is rock 1 paper 2 scissors 3,
 // Do for computer ONLY:
-// if computer 1 and user 1, draw
+//////// if computer 1 and user 1, draw
 // if computer 1 and user 2, user wins
 // if computer 1 and user 3, computer wins
-// if computer 2 and user 1, user wins 
-// if computer 2 and user 2, draw
-// if computer 2 and user 3, computer wins
+// if computer 2 and user 1, computer wins 
+//////// if computer 2 and user 2, draw
+// if computer 2 and user 3, user wins
 // if computer 3 and user 1, computer wins
 // if computer 3 and user 2, user wins
-// if computer 3 and user 3, draw
+//////// if computer 3 and user 3, draw
 
 // RANDOM NUMBER GENERATOR FOR THE COMPUTER // 
 
@@ -26,11 +26,18 @@ const computer = randomIntFromInterval(1,3)
 
 const player = Number(prompt("Type 1 for Rock, 2 for Paper and 3 for Scissors!"))
 
+console.log(computer)
 console.log(player)
 
 // COMPUTER ROCK (1) //
 
-if (computer == 1 && player == 1) {
-    console.log("Draw.")
+if (computer == 1 && player == 2) {
+    console.log("You win, paper beats rock!")
+}
+if (computer == 1 && player == 3) {
+    console.log("You lose, rock beats scissors!")
+}
+if (computer == 2 && player == 1) {
+    console.log("You lose, paper beats rock!")
 }
 
