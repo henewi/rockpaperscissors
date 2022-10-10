635a5b135ee4657ebe236c4eb8c64d398426af95
henewi <hpdunn2002@gmail.com>
2022-10-10 16:00:37 +0100

    rules written

---
+++ javascript.js
@@ -0,0 +1,15 @@
+// Define the function 
+// function to choose number between 1 and 3 
+// If number is 1 rock 2 paper 3 scissors
+// Ask user for input rock paper or scissors
+// If user is rock 1 paper 2 scissors 3,
+// Do for computer ONLY:
+// if computer 1 and user 1, draw
+// if computer 1 and user 2, user wins
+// if computer 1 and user 3, computer wins
+// if computer 2 and user 1, user wins 
+// if computer 2 and user 2, draw
+// if computer 2 and user 3, computer wins
+// if computer 3 and user 1, computer wins
+// if computer 3 and user 2, user wins
+// if computer 3 and user 3, draw
