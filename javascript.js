210649d6ae8bf6425d67cb4f3d717b50805a2ba5
henewi <hpdunn2002@gmail.com>
2022-10-10 16:21:30 +0100

    player's number input added

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
-// Define the function for AI gen number
-// function to choose number between 1 and 3 
+/// Define the function for AI gen number
+/// function to choose number between 1 and 3 
 // If number is 1 rock 2 paper 3 scissors
 // Ask user for input rock paper or scissors
 // If user is rock 1 paper 2 scissors 3,
@@ -14,8 +14,18 @@
 // if computer 3 and user 2, user wins
 // if computer 3 and user 3, draw
 
+// RANDOM NUMBER GENERATOR FOR THE COMPUTER // 
+
 function randomIntFromInterval(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 const computer = randomIntFromInterval(1,3)
+
+// NUMBER FROM THE player //
+
+const player = Number(prompt("Type 1 for Rock, 2 for Paper and 3 for Scissors!"))
+
+console.log(player)
+
+// COMPUTER ROCK (1) //
