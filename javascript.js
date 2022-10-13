30452200e50dbea6e97906be5f94fe0b75bda57d
henewi <hpdunn2002@gmail.com>
2022-10-13 14:18:00 +0100

    rm top //

--- javascript.js
+++ javascript.js
@@ -1,18 +1,3 @@
-/// Define the function for AI gen number
-/// function to choose number between 1 and 3 
-// If number is 1 rock 2 paper 3 scissors
-// Ask user for input rock paper or scissors
-// If user is rock 1 paper 2 scissors 3,
-// Do for computer ONLY:
-//////// if computer 1 and user 1, draw
-// if computer 1 and user 2, user wins
-// if computer 1 and user 3, computer wins
-// if computer 2 and user 1, computer wins 
-//////// if computer 2 and user 2, draw
-// if computer 2 and user 3, user wins
-// if computer 3 and user 1, computer wins
-// if computer 3 and user 2, user wins
-//////// if computer 3 and user 3, draw
 
 // RANDOM NUMBER GENERATOR FOR THE COMPUTER // 
 
